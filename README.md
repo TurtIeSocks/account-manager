86c63c2ad5697645311ce9de471eeb7da185322f
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2023-08-29 16:54:38 -0400

    initial

---
+++ README.md
@@ -0,0 +1,36 @@
+# Kinan Account Manager
+
+## Description
+
+Basic JS script to move accounts around depending on status and report stats to a Discord webhook.
+
+## Requirements
+
+1. NodeJS 18+
+2. MySQL
+
+## Setup
+
+1. Clone the repo
+2. Install dependencies with `npm install`
+3. Fill out config `cp config/default.json config/local.json`
+4. Run with `npm start`
+5. (Optional) Run on a cron schedule with PM2 `pm2 start ./src/index.js --name accounts --cron "*/30 * * * *"`
+
+## Config
+
+### `discordWebhookUrl`
+
+A Discord webhook address to send reports to.
+
+### `kinanOutputFolder`
+
+The _relative_ path from the directory that you're executing this script. So if you're in the root of this project, and your output is in `../kinan/output`, you would put that here.
+
+## `levelerDb`
+
+The database for your dedicated leveler instance. If you aren't using a dedicated leveler, you probably don't need this script. The `reloadUrl` key is if you want to call an API to refresh the accounts in memory, or something.
+
+## `destinationDbs`
+
+You can put any number of databases here that you wish to distribute accounts to. You must set the `ratio` for every database. So if you want to split them evenly between two destinations you would put `0.5` for each. If you want to send 1/3 of the accounts to one destination and 2/3 to another, you would put `0.333` and `0.666` respectively.
---
+++ src/index.js
@@ -0,0 +1,226 @@
+// @ts-check
+const fs = require('fs')
+const path = require('path')
+const { knex } = require('knex')
+const config = require('config')
+const { cwd } = require('process')
+
+const STATS_FILE = path.resolve(__dirname, '../.cache/stats.json')
+const ACCOUNTS_FILE = path.resolve(__dirname, '../.cache/accounts.csv')
+const KINAN_OUTPUT_FOLDER = path.resolve(cwd(), config.get('kinanOutputFolder'))
+
+/**
+ * Wrapped around `fetch` with an abort controller and error catcher
+ * @param {string} url
+ * @param {RequestInit & { waitTime?: number }} [options]
+ */
+const fetchWrapper = async (
+  url,
+  { waitTime, ...options } = { waitTime: 5000 }
+) => {
+  const signal = new AbortController()
+  const timeout = setTimeout(() => signal.abort(), waitTime)
+  try {
+    const res = await fetch(url, {
+      ...options,
+      signal: signal.signal,
+    })
+    return res
+  } catch (e) {
+    console.error(e)
+    return { status: 500 }
+  } finally {
+    clearTimeout(timeout)
+  }
+}
+
+async function main() {
+  const { reloadUrl: levelerReload, ...levelerDb } = config.get('levelerDb')
+
+  const leveler = knex({
+    client: 'mysql2',
+    connection: levelerDb,
+  })
+
+  const reloadUrls = levelerReload ? [levelerReload] : []
+
+  /**
+   * @type {{ connection: typeof leveler, ratio: number, name: string }[]}
+   */
+  const destinationDbs = config
+    .get('destinationDbs')
+    .map(({ reloadUrl, ratio, ...db }) => {
+      if (reloadUrl) reloadUrls.push(reloadUrl)
+      return {
+        connection: knex({ client: 'mysql2', connection: db }),
+        ratio,
+        name: db.database,
+      }
+    })
+
+  const stats = {
+    newAccounts: 0,
+    newThirties: 0,
+    timestamp: Date.now(),
+  }
+
+  const allAccounts = fs.readdirSync(KINAN_OUTPUT_FOLDER).flatMap((file) => {
+    const lines = fs
+      .readFileSync(path.resolve(KINAN_OUTPUT_FOLDER, file), 'utf-8')
+      .split('\n')
+      .filter((l) => !l.startsWith('#') && l.endsWith('OK;'))
+    return lines.map((line) => {
+      const [user, pass, email] = line.split(';')
+      return `${user},${pass},${email}`
+    })
+  })
+
+  const existingAccounts = new Set(
+    (fs.existsSync(ACCOUNTS_FILE)
+      ? fs.readFileSync(ACCOUNTS_FILE, 'utf-8').split('\n')
+      : []
+    ).map((row) => {
+      const [username] = row.split(',')
+      return username.trim()
+    })
+  )
+
+  fs.writeFileSync(path.resolve(ACCOUNTS_FILE), allAccounts.join('\n'), 'utf8')
+
+  const newAccounts = allAccounts.filter((account) => {
+    const [username] = account.split(',')
+    return !existingAccounts.has(username)
+  })
+  stats.newAccounts = newAccounts.length
+  console.log('Made', stats.newAccounts, 'new accounts')
+
+  const accountsForDb = newAccounts.map((account) => {
+    const [username, password] = account.split(',')
+    return { username, password, level: 0 }
+  })
+
+  if (accountsForDb.length) {
+    await leveler('account')
+      .insert(accountsForDb)
+      .onConflict('username')
+      .ignore()
+  }
+
+  const newThirties = await leveler('account')
+    .where('banned', '=', 0)
+    .where('level', '>', 29)
+  stats.newThirties = newThirties.length
+  console.log(newThirties.length, 'ready for use!')
+
+  if (newThirties.length) {
+    await leveler('account')
+      .update({ banned: true })
+      .whereIn(
+        'username',
+        newThirties.map((account) => account.username)
+      )
+
+    for (const { connection, ratio, name } of destinationDbs) {
+      const isLast = destinationDbs[destinationDbs.length - 1]?.name === name
+      const accounts = isLast
+        ? newThirties
+        : newThirties.splice(0, Math.floor(newThirties.length * ratio))
+
+      if (accounts.length) {
+        stats[`new${name}`] = accounts.length
+        await connection('account')
+          .insert(accounts)
+          .onConflict('username')
+          .ignore()
+        console.log('Inserted', accounts.length, 'into', name)
+      }
+    }
+  }
+
+  /** @type {(typeof stats)[]} */
+  const existingStats = fs.existsSync(STATS_FILE)
+    ? JSON.parse(fs.readFileSync(STATS_FILE, 'utf-8'))
+    : []
+
+  existingStats.push(stats)
+
+  fs.writeFileSync(STATS_FILE, JSON.stringify(existingStats))
+
+  const goodThirties = Object.fromEntries(
+    await Promise.all(
+      destinationDbs.map(async ({ connection, name }) => {
+        const goodThirties = await connection('account')
+          .where('level', '>', 29)
+          .andWhere('banned', 0)
+          .andWhere('invalid', 0)
+          .whereNull('last_disabled')
+          .count('username', { as: 'total' })
+          .first()
+        return [name, goodThirties?.total ?? 0]
+      })
+    )
+  )
+
+  if (config.get('discordWebhookurl')) {
+    await fetchWrapper(config.get('discordWebhookurl'), {
+      waitTime: 5000,
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        content: null,
+        embeds: [
+          {
+            title: 'Leveling Stats',
+            color: 5814783,
+            fields: [
+              {
+                name: 'Number of Level 0s Created',
+                value: `${stats.newAccounts}`,
+              },
+              {
+                name: 'Number of Level 30s Created',
+                value: `${stats.newThirties}`,
+              },
+              ...destinationDbs.map(({ name }) => ({
+                name: `Number of Level 30s Added to ${name}`,
+                value: `${stats[`new${name}`] || 0}`,
+              })),
+            ],
+            timestamp: new Date().toISOString(),
+          },
+          {
+            title: 'Fresh Accounts',
+            color: 5814783,
+            fields: Object.entries(goodThirties).map(([name, total]) => ({
+              name: `Number of Level 30s in ${name}`,
+              value: `${total}`,
+            })),
+            timestamp: new Date().toISOString(),
+          },
+        ],
+        attachments: [],
+      }),
+    }).then((res) => console.log('Webhook status', res?.status))
+  }
+  
+  await Promise.all(
+    destinationDbs.map(async (db) => await db.connection.destroy())
+  )
+  await leveler.destroy()
+
+  const date = new Date()
+  if (date.getHours() === 4 && date.getMinutes() < 10) {
+    // only reload once at midnight
+    await Promise.allSettled(
+      reloadUrls.map((url) =>
+        fetchWrapper(url)
+          .then((res) => console.log(url, res?.status))
+          .catch((err) => console.error(url, err))
+      )
+    )
+  }
+}
+
+main().then(() => console.log('OK!'))
